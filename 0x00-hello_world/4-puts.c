38ff13a3e52f8104996c3902e1d7653c171329bf
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-03-16 17:08:40 -0700

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-
-int main(void)
+/**
+ * main function - Print exact message without prinf
+ *
+ * Return: program should end with 0.
+ */
+int main()
 {
-	char message[] = "\"Programming is like building a multilingual puzzle";
-	
+	char message[] = "\"Programming is like building a multilingual puzzle";	
 	puts(message);
-	return 0;
+	return (0);
 }
