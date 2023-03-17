bb4569fb576a6bf3b753e25a9bdec105113c7549
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-03-16 17:51:35 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 const char str[] = "and that piece of art is useful\""
-       "- Dora Korpar, 2015-10-19\n";
++" - Dora Korpar, 2015-10-19\n";
 ssize_t len = sizeof(str) - 1;
 
 /* Use the write system call to print to stderr */
