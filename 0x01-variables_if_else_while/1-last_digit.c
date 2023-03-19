19b0a5ef1691832aa98a42cf2855a012ac12995b
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-03-19 16:24:42 -0700

    Based on last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
 /**
  * main - entry pint of the program
  *
- * Description: Assigns a random value to the variable n and prints a result 
+ * Description: Assigns a random value to the variable n and prints a result
  * based on the last digit of n
  *
  * Return: 0 on success
@@ -20,15 +20,15 @@ n = rand() - RAND_MAX / 2;
 printf("Last digit of %d is", n);
 if (n % 5 <= 4 && n % 5 > 0)
 {
-printf(" and is greater than 5 \n");
+printf(" %d and is greater than 5 \n", ((n % 5) + 5 ));
 }
 else if (n % 10 == 0)
 {
-printf(" and is 0 \n");
+printf(" %d and is 0 \n", 0);
 }
 else if (n % 6 <= 5 && n % 10 > 0)
 {
-printf(" and is less than 6 and not 0");
+printf(" %d and is less than 6 and not 0", ((n % 6) + 6));
 }
 return (0);
 }
