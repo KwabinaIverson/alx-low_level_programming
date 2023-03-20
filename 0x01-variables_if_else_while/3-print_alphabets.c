418e8a28c1d8433f57f240fcc37f23f97c5eef1f
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-03-20 13:40:00 -0700

    Print Alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,33 +9,36 @@
  */
 int main(void)
 {
-char letterA[] = "abcdefghijklmnopqr";
+char letterA[] = "abcderghijklmnopqr";
 char letterB[] = "stuvwxyzABCDEFGHIJK";
 char letterC[] = "LMNOPQRSTUVWXYZ";
-int i = 0;
 int lengthA = sizeof(letterA) / sizeof(letterA[0]);
 int lengthB = sizeof(letterB) / sizeof(letterB[0]);
 int lengthC = sizeof(letterC) / sizeof(letterC[0]);
+int i = 0;
 while (i <= lengthA - 1)
 {
 putchar(letterA[i]);
 i++;
+}
 if (i == 19)
 {
 i = 0;
-while (i <= lengthB -1)
+while (i <= lengthB - 1)
 {
 putchar(letterB[i]);
 i++;
+}
 if (i == 20)
 {
 i = 0;
 while (i <= lengthC - 1)
+{
 putchar(letterC[i]);
 i++;
 }
 }
 }
-}
+
 return (0);
 }
