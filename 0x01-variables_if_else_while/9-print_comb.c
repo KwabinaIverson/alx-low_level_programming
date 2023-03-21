59de088117e8c7a709707ae93907bbbd04a6084f
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-03-20 19:35:26 -0700

    Combination of singled-digit number

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,18 +12,27 @@ int main(void)
 int i = 0;
 while (i < 10)
 {
+int i = 0;
+while (i < 10)
+{
 int j = 0;
 while (j < 10)
 {
-putchar(i + '0');
+putchar(i * 10 + j + '0');
+if (i == 9 && j == 9)
+{
+putchar('\n');
+}
+else
+{
 putchar(',');
 putchar(' ');
-putchar(j + '0');
-putchar('\n');
+}
 j++;
 }
 i++;
 }
+
 return (0);
 }
 
