4820b304822bf51b9a98d1d2b72ffeeca449a9f2
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-03-20 18:34:30 -0700

    Print 0 - 9. Don't use char data type

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Description: Print from 0 - 9 using putchar
+ *
+ * Return: 0 is success
+ */
+int main(void)
+{
+int i = 0;
+while (i <= 9)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
