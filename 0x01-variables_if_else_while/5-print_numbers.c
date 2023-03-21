5cf53826427a741e13cfde375faada3239bcda99
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-03-20 18:12:36 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Description: Print the numbers from 0 - 9
+ *
+ * Return: 0 is success
+ */
+int main(void)
+{
+char numbers[] = "0123456789";
+printf(numbers);
+return (0);
+}
