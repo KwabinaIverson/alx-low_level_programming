48537f1d857ebe5e6baf9a3fcaa49e35d4eb284c
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-03-21 16:34:07 -0700

    Prints a-z ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the letters a-z ten times
+ *
+ * Return: The return value is void
+ */
+void print_alphabet_x10(void)
+{
+int times = 1;
+while (times <= 10)
+{
+char myChar = 'a';
+while (myChar <= 'z')
+{
+putchar(myChar);
+myChar++;
+}
+putchar('\n');
+times++;
+}
+}
