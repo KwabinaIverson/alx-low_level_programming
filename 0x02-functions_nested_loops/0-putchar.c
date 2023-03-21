c0923f6c61ab7954c4c9e95efe453387fb9908dc
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-03-21 07:02:27 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Description: Putchar prints an array of characters using while loop
+ *
+ * Return: 0 is success
+ */
+int main(void)
+{
+char myWord[] = "_putchar";
+int i = 0;
+while (i <= 7)
+{
+putchar(myWord[i]);
+i++;
+}
+putchar('\n');
+return (0);
+}
