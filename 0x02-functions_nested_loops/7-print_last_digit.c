4f6cab5a1ee3d785f35c6049cc4cbec997b5ffbe
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-03-22 04:59:26 -0700

    Print the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,17 +6,21 @@
  * @n: Get the last digit of integer n
  *
  * Description: Print the last digit of n
+ *
+ * Return: Character of last digit of n and return last digit of n
  */
 int print_last_digit(int n)
 {
 if (n % 10 >= 0 && n % 10 <= 9)
 {
 int remainder = n % 10;
+_putchar(remainder);
 return (remainder);
 }
-else
+else if (n % 10 < 0)
 {
 int remain = n % 10;
+_putchar(remain);
 return (remain);
 }
 }
