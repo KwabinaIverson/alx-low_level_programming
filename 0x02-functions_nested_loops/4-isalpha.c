7cc7149fb479e83112efd730311db58629aaa30a
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-03-22 17:06:18 -0700

    Return 1 if a character is a letter, else return 0

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - return type of int and takes int type a an argument
+ *
+ * @c: is the character to be checked against conditional statements
+ *
+ * Description: Return 1 if c is or among a-z and A-Z, else return 0
+ *
+ * Return: Returns an int type
+ */
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
