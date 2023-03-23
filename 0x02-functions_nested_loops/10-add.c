39e3b42b8b283680d01c36fd59c40a78ed0d62f8
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-03-22 17:56:29 -0700

    Add two numbers and return the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - returns int type and takes two int type arguments
+ *
+ * @num1, @num2: The integers to be added
+ *
+ * Description: Adds integers an returns the result
+ *
+ * Return: Return two added int types
+ */
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
