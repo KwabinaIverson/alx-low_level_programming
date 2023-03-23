42332be750d9f3782b70e7e4e482f0bad7d48d54
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-03-22 19:00:33 -0700

    Print natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ if ( n <= 98)
 while (n <= 98)
 {
 _putchar(n + '0');
-n++;
+in++;
 }
 }
 else if (n >= 98)
@@ -26,8 +26,9 @@ while (n >= 98)
 _putchar(n + '0');
 n--;
 }
+}
 else
 {
-_putchar(n + '0')
+_putchar(n + '0');
 }
 }
