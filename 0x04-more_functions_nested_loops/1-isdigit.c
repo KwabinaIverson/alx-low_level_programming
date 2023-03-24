d33725be95b38a5bb82d2df48eaab3682ec5121b
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-03-23 18:50:53 -0700

    Is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - return an int type if c is a digit
+ *
+ * @c: The digit to check
+ *
+ * Return: 1 if its digit else 0
+ */
+int _isdigit(int c)
+{
+while (c >= 0 && c <= 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
