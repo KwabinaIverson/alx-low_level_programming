0a571786720ecda26c48fa5590ecec23cf6049a3
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-03-24 19:35:06 -0700

    Print in diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_diagonal - print in diagonal
+ *
+ * @n: Number of prints
+ */
+void print_diagonal(int n)
+{
+if (n <= 0) {
+putchar('\n');
+return;
+}
+for (int i = 0; i < n; i++)
+{
+for (int j = 0; j < i; j++)
+{
+putchar(' ');
+}
+putchar('\\');
+putchar('\n');
+}
+}
