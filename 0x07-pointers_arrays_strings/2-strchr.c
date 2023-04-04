124850d294599dbd1a04bd370ac1a31f0ded4ff9
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-04-03 18:36:27 -0700

    Strchr in C

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - finds a charcter in a string
+ * @s: The string to search
+ * @c: The character to search
+ * Return: s or NULL
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c) {return s;
+}
+s++;
+}
+if (c == '\0')
+{
+return s;
+}
+return NULL;
+}
