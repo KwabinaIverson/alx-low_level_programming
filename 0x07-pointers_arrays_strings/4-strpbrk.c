535fc9583cfb9e75f6788b8fa621aad7b3f87b43
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-04-04 17:39:20 -0700

    Strpbrk in C

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,18 +1,18 @@
 #include "main.h"
-#include <stddef>
+#include <stddef.h>
 
 /**
- * _strpbrk - searches for char accept in char *s
- * @s: The string to search in
- * @accept: The string to search for
- * Return: NULL
+ * _strpbrk - searches char s for char accept
+ * @s: The String used
+ * @accept: The string used for search
+ * Return: Pointer to s or NULL
  */
 char *_strpbrk(char *s, char *accept)
 {
-char *s_ptr, *accept_ptr;
-for (*s_ptr = s; *s_ptr != '\0'; *s_ptr++)
+const char *s_ptr, *accept_ptr;
+for (s_ptr = s; *s_ptr != '\0'; s_ptr++)
 {
-for (*accept_ptr = accept; *accept_ptr != '\0'; *accept_ptr++)
+for (accept_ptr = accept; *accept_ptr != '\0'; accept_ptr++)
 {
 if (*s_ptr == *accept_ptr)
 {
@@ -20,5 +20,5 @@ return (char *)s_ptr;
 }
 }
 }
-return (NULL);
+return NULL;
 }
