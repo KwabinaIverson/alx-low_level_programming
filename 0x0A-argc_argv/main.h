7fd57ab6373a02e65854340fb9f5de2f5a6c2f1f
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-04-10 06:36:20 -0700

    Readme file and _putchar file

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+
+void func_name(int argc, char *argv[]);
+void num_arg(int argc, char *argv[]);
+void print_arg(int argc, char *argv[]);
+int multiply(int a, int b);
+int add(int a, int b);
+int _putchar(char c);
+
+#endif
