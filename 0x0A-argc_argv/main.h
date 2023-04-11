52ed5ce153b4a400a86746d67c5ddc2b410dc404
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-04-10 17:46:45 -0700

    Main file

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,11 +1,7 @@
 #ifndef main_h
 #define main_h
 
-void func_name(int argc, char *argv[]);
-void num_arg(int argc, char *argv[]);
-void print_arg(int argc, char *argv[]);
-int multiply(int a, int b);
-int add(int a, int b);
+int _atoi(char *s);
 int _putchar(char c);
 
 #endif
