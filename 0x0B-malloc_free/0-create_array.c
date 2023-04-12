a6704deaa0d4c67e27f7e7376c9a609427e8e12f
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-04-11 19:00:15 -0700

    Create an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,16 +9,17 @@
  */
 char *create_array(unsigned int size, char c)
 {
+unsigned int i;
 if (size == 0)
 {
 return (NULL);
 }
-char *arr = (char *) malloc(size * sizeof(char));
+char *arr = malloc(size * sizeof(char));
 if (arr == NULL)
 {
 return (NULL);
 }
-for (unsigned int i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 arr[i] = c;
 }
