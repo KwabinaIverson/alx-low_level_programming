bdb072058d4d76991043e6356dd74c6da015311f
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-04-11 19:25:30 -0700

    Concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,8 @@
 char *str_concat(char *s1, char *s2)
 {
 char *concat;
+size_t len1;
+size_t len2;
 if (s1 == NULL)
 {
 s1 = "";
@@ -19,8 +21,8 @@ if (s2 == NULL)
 {
 s2 = "";
 }
-size_t len1 = strlen(s1);
-size_t len2 = strlen(s2);
+len1 = strlen(s1);
+len2 = strlen(s2);
 concat = malloc(len1 + len2 + 1);
 if (concat == NULL)
 {
