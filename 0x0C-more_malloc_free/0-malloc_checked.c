eea36c74c232d56fe1c532cefbf5f1ebc9b77fb7
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-04-13 18:18:43 -0700

    Dynamic allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - dynamically allocates memory
+ * @b: The number of bytes
+ * Return: A pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr = mallac(b);
+if (ptr == NULL)
+{
+exit(98);
+}
+return (ptr);
+}
