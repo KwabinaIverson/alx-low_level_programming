a1aa3637345832858368c4cbbe3d4015b47d643d
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-04-19 22:19:55 -0700

    Print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
  * print_name - print name
@@ -7,5 +8,9 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+if (name == NULL || f == NULL)
+{
+return;
+}
 f(name);
 }
