c859035f6b0148572ee83dc7323c6b4a65cea96e
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-04-27 06:15:51 -0700

    Add element to linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node - add node to a linked list
+ * @head: Pointer to the head list
+ * @str: String to add
+ *
+ * Return: address of element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+    list_t *new_node;
+
+    if (head == NULL || str == NULL)
+        return (NULL);
+
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->len = strlen(str);
+    new_node->next = *head;
+    *head = new_node;
+
+    return (new_node);
+}
