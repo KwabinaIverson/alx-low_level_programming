046109deca2b423905ffcc6083fb4e66185d4252
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-05-02 00:28:21 -0700

    List elements

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t count = 0;
+	size_t element = 0;
 
 	while (h != NULL)
 	{
-		count++;
+		element++;
 		h = h->next;
 	}
 
-	return (count);
+	return (element);
 }
