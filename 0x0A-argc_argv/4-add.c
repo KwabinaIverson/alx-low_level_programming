2d9f74211560fee635ad88d9802c586cff56a27e
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-05-02 23:30:29 -0700

    Add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+int main(int argc, char *argv[])
+{
+int sum = 0, i;
+if (argc < 2)
+{
+printf("0\n");
+return 0;
+}
+for (i = 1; i < argc; i++)
+{
+int j = 0;
+while (argv[i][j])
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return 1;
+}
+j++;
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+return 0;
+}
