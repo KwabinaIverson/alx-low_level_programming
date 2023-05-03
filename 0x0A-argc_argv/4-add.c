df2e4b4bb5f7156ac662ff4f754e8f8701ea3cc1
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-05-02 23:33:16 -0700

    Add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,7 +9,7 @@ int sum = 0, i;
 if (argc < 2)
 {
 printf("0\n");
-return 0;
+return (0);
 }
 for (i = 1; i < argc; i++)
 {
@@ -19,12 +19,12 @@ while (argv[i][j])
 if (!isdigit(argv[i][j]))
 {
 printf("Error\n");
-return 1;
+return (1);
 }
 j++;
 }
 sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
-return 0;
+return (0);
 }
