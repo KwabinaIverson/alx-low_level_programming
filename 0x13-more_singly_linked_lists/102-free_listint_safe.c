a3c1f1d2ca1b8bfdd3e93a6c7307e8ffff5c67cc
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-05-03 23:09:07 -0700

    Free

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,53 @@
 #include "lists.h"
 
+size_t looped_listint_count(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+
+/**
+ * looped_listint_count - counts looped listint
+ * @head: Pointer to the head node
+ *
+ * Return: Node address
+ */
+size_t looped_listint_count(listint_t *head)
+{
+        listint_t *t = head, *h = head;
+        size_t node = 1;
+
+        if (head == NULL || head->next == NULL)
+        {
+                return (0);
+        }
+
+        while (h && h->next)
+        {
+                t = t->next;
+                h = (h->next)->next;
+
+                if (t == h)
+                {
+                        t = head;
+                        while (t != h)
+                        {
+                                node++;
+                                t = t->next;
+                                h = h->next;
+                        }
+
+                        t = t->next;
+                        while (t != h)
+                        {
+                                node++;
+                                t = t->next;
+                        }
+
+                        return (node);
+                }
+        }
+
+        return (0);
+}
+
 /**
  * free_listint_safe - Frees a listint_t list.
  * @h: A pointer to the head of the listint_t list.
@@ -8,31 +56,29 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t length;
-	int i;
-	listint_t *ptr;
+        size_t nodes, ind;
+        listint_t *mp;
 
-	if (!h || !*h)
-		return (0);
-	while (*h)
-	{
-		i = *h - (*h)->next;
-		if (i > 0)
-		{
-			ptr = (*h)->next;
-			free(*h);
-			*h = ptr;
-			length++;
-		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-			length++;
-			break;
-		}
-	}
+        nodes = looped_listint_count(*h);
 
-	*h = NULL;
-	return (length);
+        if (nodes == 0)
+        {
+                for (; h != NULL && *h != NULL; nodes++)
+                {
+                        mp = (*h)->next;
+                        free(*h);
+                        *h = mp;
+                }
+        }
+        else
+        {
+                for (ind = 0; ind < nodes; ind++)
+                {
+                        mp = (*h)->next;
+                        free(*h);
+                        *h = mp;
+                }
+        }
+
+        return (nodes);
 }
