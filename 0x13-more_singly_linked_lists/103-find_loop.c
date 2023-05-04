749d3b930c80a7b351ad536c5e0ded2309b6cff0
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-05-03 22:53:47 -0700

    Find

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -8,26 +8,21 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *s = head, *f = head;
-
-    while (s && f && f->next)
-    {
-        s = s->next;
-        f = f->next->next;
-
-        if (s == f)
-	{
-            s = head;
-
-            while (s != f)
-	    {
-                s = s->next;
-                f = f->next;
-            }
-
-            return s;
-        }
-    }
-
-    return NULL;
+listint_t *s = head, *f = head;
+while (s && f && f->next)
+{
+s = s->next;
+f = f->next->next;
+if (s == f)
+{
+s = head;
+while (s != f)
+{
+s = s->next;
+f = f->next;
+}
+return s;
+}
+}
+return NULL;
 }
