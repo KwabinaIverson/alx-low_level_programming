21385d539859f458f400949be96f0386cdbc6e49
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-05-04 17:46:54 -0700

    Convert bits to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	while (*b != '\0')
 	{
-		if (*b >= 'a' && *b <= 'z')
+		if (*b != '0' && *b != '1')
 			return (0);
 		result = (result << 1) + (*b++ - '0');
 	}
