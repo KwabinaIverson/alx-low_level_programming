42a22a3a453231b76a92232664cd933ce4be0c6d
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-05-10 03:19:34 -0700

    Create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/**
+ * create_file - create file with the file name and puts conten in it
+ * @filename: The name of the file to create
+ * @text_content: Content to put in the file
+ *
+ * Return: An integer 1 (Success)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t content_length, bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		content_lenght = strlen(text_content);
+		bytes_written = write(fd, text_content, content_lenght);
+		if (bytes_written == -1 || bytes_written != content_lenght)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
