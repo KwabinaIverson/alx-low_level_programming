d95d764f0e6602370ffced9141f2a000c34bdfb0
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-05-12 19:01:33 -0700

    Append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,22 +14,22 @@ int append_text_to_file(const char *filename, char *text_content)
 	FILE *file;
 
 	if (filename == NULL)
-		return -1;
+		return (-1);
 
 	if (text_content == NULL)
-		return -1;
+		return (-1);
 
 	file = fopen(filename, "a");
 	if (file == NULL)
-		return -1;
+		return (-1);
 
 	result = fputs(text_content, file);
 	if (result == EOF)
 	{
 		fclose(file);
-		return -1;
+		return (-1);
 	}
 
 	fclose(file);
-	return 1;
+	return (1);
 }
