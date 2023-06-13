9e2582ba040d63bd87c53b7f7f82db37ff64ab6c
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-06-13 07:52:57 -0700

    String duplicate function (str)

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - duplicates a string
+ * @str: String to duplicate
+ *
+ * Return: Pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	size_t len;
+
+	if (str == NULL)
+		return (NULL);
+	len = strlen(str) + 1;
+	dup = malloc(sizeof(char) * len);
+
+	memcpy(dup, str, len * sizeof(char));
+	return (dup);
+}
