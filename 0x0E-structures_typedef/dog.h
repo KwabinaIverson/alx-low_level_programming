a3940d0969cf5e80e7e07e3b5781b658bc74d548
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-06-19 19:24:09 -0700

    Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,11 +19,11 @@ struct dog
 /**
  * dot_t - an instance of struct do
  */
-typedef struct dog dog_t;
+/* typedef struct dog dog_t; */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+/*void init_dog(struct dog *d, char *name, float age, char *owner);*/
+/*void print_dog(struct dog *d);*/
+/*dog_t *new_dog(char *name, float age, char *owner);*/
+/*void free_dog(dog_t *d);*/
 
 #endif /* DOG_H */
