74f43d696026c6b3d2e6af695f3d77e4d04564db
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-06-19 19:19:32 -0700

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,24 @@
 #define DOG_H
 
 /**
- * dog - a structure data type in C that has 3 members of elements
+ * struct dog - a structure data type in C that has 3 members of elements
+ * @name: First element of struct dog
+ * @age: Second element of struct dog
+ * @owner: Third element of struct dog
+ *
+ * Description: This data type define variable name, age, and owner.
  */
-struct dog {
+struct dog
+{
 	char *name;
 	float *age;
 	char *owner;
 };
+
+/**
+ * dot_t - an instance of struct do
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
