46fdc3f62e0ce38d209354e7713b7730e78bad9f
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-06-21 16:59:18 -0700

    Array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointer.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - loop through arrays
+ * @array: Pointer array to loop
+ * @size: Size of array
+ * @action: Pointer function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int a;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (a = 0; a < size; a++)
+		action(array[a]);
+}
