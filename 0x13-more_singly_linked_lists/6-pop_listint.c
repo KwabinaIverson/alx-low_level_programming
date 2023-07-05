c2c6cd419ffdc4aa6fee14f86757c33f3fed217d
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-07-04 18:43:46 -0700

    Pop from the top of the list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pop from the top of the list
+ * @head: Pointer pointer tot the head address
+ *
+ * Return: Integer
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int num;
+
+	if (!head || !*head)
+		return (0);
+
+	num = (*head)->n;
+	tmp = (*head)->next;
+	free(*head);
+	*head = tmp;
+
+	return (num);
+}
