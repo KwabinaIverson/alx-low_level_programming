6f78d6227664a1b0502b5518070d0f3869f918bf
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-07-05 16:53:30 -0700

    Reverse Listint_t

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse listint_t
+ * @head: Initial head address
+ *
+ * Return: New head address
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+
+	return (*head);
+}
