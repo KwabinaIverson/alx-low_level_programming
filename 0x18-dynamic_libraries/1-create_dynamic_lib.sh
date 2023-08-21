b6a95fd05567d4cdaef2eb358c41d9d8894d6ca1
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-08-21 09:22:40 -0700

    Corection made

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-gcc -fpic *.c -c
+# Gets all C files in current directory
+c_file=$(ls *.c)
+
+# Create object file by looping through each file
+for c_file in $c_file; do
+	gcc -fpic c_file -c
+done
+
+# Create dynamic library
 gcc -shared -o liball.so *.o
