1a5f4ef2334e332a45cfb4932e7d1bf900d6ae84
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-09-08 19:17:50 -0700

    Print from the hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -3,26 +3,28 @@
 /**
  * hash_table_print - prints elements in the hash table
  * @ht: The address to the hash table
-*/
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned int i;
 	hash_node_t *item;
+	int first = 1;
 
 	if (ht == NULL)
-	{
-		printf("{}");
 		return;
-	}
-    
+
+	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		item = ht->array[i];
-		while (item != NULL)
+		while (item)
 		{
-			printf("{'%s': '%s'}, ", item->key, item->value);
+			if (!first)
+				printf(", ");
+			printf("'%s': '%s'", item->key, item->value);
+			first = 0;
 			item = item->next;
 		}
 	}
-	printf("\n");
+	printf("}\n");   
 }
