9bd49b8d2770a9e00d756f8cff74a655f3fd0f51
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-10-29 21:38:20 -0700

    Fixed

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,35 +1,28 @@
 #!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
+
 
 def island_perimeter(grid):
-    if not grid:
-        return 0
+    """Return the perimiter of an island.
 
-    perimeter = 0
+    The grid represents water by 0 and land by 1.
 
-    # Iterate through the rows and columns of the grid
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[row][col] == 1:
-                # For each land cell, check its adjacent cells
-                neighbors = 0
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-                # Check the cell to the left
-                if col > 0 and grid[row][col - 1] == 1:
-                    neighbors += 1
-
-                # Check the cell to the right
-                if col < len(grid[0]) - 1 and grid[row][col + 1] == 1:
-                    neighbors += 1
-
-                # Check the cell above
-                if row > 0 and grid[row - 1][col] == 1:
-                    neighbors += 1
-
-                # Check the cell below
-                if row < len(grid) - 1 and grid[row + 1][col] == 1:
-                    neighbors += 1
-
-                # The perimeter for the current land cell is 4 (total sides) minus the number of adjacent land cells
-                perimeter += (4 - neighbors)
-
-    return perimeter
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
