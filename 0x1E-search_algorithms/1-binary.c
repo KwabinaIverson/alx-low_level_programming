750ef9928c39ea2f83e95b261770b27a20618e62
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-12-13 14:13:14 -0800

    Task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * @array: pointer to the first element of the array
+ * @size: the number of elements in array
+ * @value: the value to search for
+ * Return: the index where value is located
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int low = 0;
+	int high = size - 1;
+	int mid, i;
+
+	if (array == NULL)
+		return (-1);
+
+	while (low <= high)
+	{
+		mid = low + (high - low) / 2;
+
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+			printf("%d, ", array[i]);
+		printf("\n");
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+			high = mid - 1;
+		else
+			low = mid + 1;
+	}
+	return (-1);
+}
